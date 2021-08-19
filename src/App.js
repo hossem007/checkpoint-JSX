00d1d90877f5520ed3b3591b1a105440f8d6dbd5
hossem007 <hossemeddine01990@gmail.com>
2021-08-19 11:34:30 +0100

    fs1

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  <div className="App">
+
+      <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+
+            <h1 className="title red" style={{color:"red"}}>Bahloul Hossem Eddine</h1>
+
+              <br/ >
+
+            <img src="/imageInSrc.png" alt='' />
+
+              <br/ >
+
+            <img src="/imageInPublic.png" alt=''/>
+
+      </div>
+
+            <video width="320" height="240" controls/>
+
+            <source src="myVideo.mp4" type="video/mp4"/>
+
+           
+  </div>
   );
 }
 
